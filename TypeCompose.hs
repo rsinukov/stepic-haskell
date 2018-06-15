32a5d48b10d3c49984aceb9867829fe6d8f15b92
rustam.siniukov <rxsinukov@gmail.com>
2018-06-15 17:49:06 +0200

    Cmps3 as Functor

--- TypeCompose.hs
+++ TypeCompose.hs
@@ -16,3 +16,11 @@ b = Cmps (True, id, Right 1)
 
 c :: C
 c  = Cmps (\b a -> if b then a else (a + 1))
+
+
+
+newtype Cmps3 f g h a = Cmps3 { getCmps3 :: f (g (h a)) } 
+  deriving (Eq,Show) 
+
+instance (Functor f, Functor g, Functor h) => Functor (Cmps3 f g h) where
+  fmap q (Cmps3 x) = Cmps3 $ fmap (fmap (fmap q)) x
