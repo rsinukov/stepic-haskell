eb16fd91d8a02e48dc6511144dd9752a00fa8aa8
rustam.siniukov <rxsinukov@gmail.com>
2018-06-15 17:33:34 +0200

    Type compose operator

---
+++ TypeCompose.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE TypeOperators #-}
+
+infixr 9 |.|
+newtype (|.|) f g a = Cmps { getCmps :: f (g a) } 
+    deriving (Eq, Show)
+
+type A   = ((,) Integer |.| (,) Char) Bool
+type B t = ((,,) Bool (t -> t) |.| Either String) Int
+type C   = (|.|) ((->) Bool) ((->) Integer) Integer
+
+a :: A
+a = Cmps (1, ('a', True))
+
+b :: B t
+b = Cmps (True, id, Right 1)
+
+c :: C
+c  = Cmps (\b a -> if b then a else (a + 1))
